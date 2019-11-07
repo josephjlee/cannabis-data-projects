5599a5693259acab1889b8ed618d9cfd5474a6f3
Martin Frigaard <mjfrigaard@gmail.com>
2019-11-07 08:27:01 -0800

    first commit

---
+++ 00-setup.R
@@ -0,0 +1,13 @@
+#=====================================================================#
+# This is code to create: 00-setup.R
+# Authored by and feedback to:
+# MIT License
+# Version:
+#=====================================================================#
+
+# ‹(•_•)› PACKAGES ––•––•––√\/––√\/––•––•––√\/––√\/––•––•––√\/––√\/  ----
+# download and install the project setup
+utils::download.file(url = "http://bit.ly/goodenuff-R", 
+                     destfile = "goodenuffR-template.Rmd")
+
+# open this file and run all chunks.
